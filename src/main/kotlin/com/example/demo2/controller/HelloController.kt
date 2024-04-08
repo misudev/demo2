cf8ea8f4b45b13c41aaaee671ba5dc9d5db0b2df
misu <jungmisu801@gmail.com>
2024-04-08 19:18:57 +0900

    fix: api end point

--- src/main/kotlin/com/example/demo2/controller/HelloController.kt
+++ src/main/kotlin/com/example/demo2/controller/HelloController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class HelloController {
 
-    @GetMapping("/hello2")
+    @GetMapping("/hello")
     fun hello(): String {
         return "Hello, World! demo2"
     }
